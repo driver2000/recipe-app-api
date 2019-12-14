d8c2bab432a17d86da358f1f639563bab48eb9fa
Daniele Iervolino <raiervol@gmail.com>
2019-12-14 13:32:35 +0000

    Add wait command

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ IMAGE_NAME ?= recipe-app
 .PHONY: build requirements.txt
 
 requirements.txt:
-	poetry run pip freeze > requirements.txt
+	poetry export -f requirements.txt --without-hashes > requirements.txt
 
 build: Dockerfile requirements.txt
     docker build -t $(IMAGE_NAME):$(VERSION) -f Dockerfile .
--- src/core/tests/test_commands.py
+++ src/core/tests/test_commands.py
@@ -5,5 +5,14 @@ from django.test import TestCase
 
 
 class TestCommands(TestCase):
-    def test_wait_for_db(self):
-        pass
+    @patch("django.db.utils.ConnectionHandler.__getitem__", return_value=True)
+    def test_wait_for_db_ready(self, mock):
+        call_command("wait_for_db")
+        self.assertEqual(mock.call_count, 1)
+
+    @patch("core.management.commands.wait_for_db.time", return_value=True)
+    def test_wait_for_db(self, mock_ts):
+        with patch("django.db.utils.ConnectionHandler.__getitem__", return_value=True) as mock_gi:
+            mock_gi.side_effect = [OperationalError] * 5 + [True]
+            call_command("wait_for_db")
+            self.assertEqual(mock_gi.call_count, 6)
